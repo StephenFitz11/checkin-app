6a3f4829db403a1b2a4e471799b5fc8fffa8c5be
Stephen Fitzsimmons <smf0211@gmail.com>
2024-06-21 15:09:56 -0500

    hotfix

--- prisma/migration_scripts/change_corral_names.ts
+++ prisma/migration_scripts/change_corral_names.ts
@@ -1,37 +1,33 @@
-const { PrismaClient } = require("@prisma/client");
+// async function chnageCorralName() {
+//   const nameMap: { [key: number]: string } = {
+//     1: "A",
+//     2: "B",
+//     3: "C",
+//     4: "D",
+//   };
 
-const prisma = new PrismaClient();
+//   const results = await prisma.coral.findMany({});
 
-async function chnageCorralName() {
-  const nameMap: { [key: number]: string } = {
-    1: "A",
-    2: "B",
-    3: "C",
-    4: "D",
-  };
+//   for (let index = 0; index < results.length; index++) {
+//     const element = results[index];
 
-  const results = await prisma.coral.findMany({});
+//     await prisma.coral.update({
+//       where: {
+//         id: element.id,
+//       },
+//       data: {
+//         name: nameMap[element.id as number],
+//       },
+//     });
+//   }
+// }
 
-  for (let index = 0; index < results.length; index++) {
-    const element = results[index];
-
-    await prisma.coral.update({
-      where: {
-        id: element.id,
-      },
-      data: {
-        name: nameMap[element.id as number],
-      },
-    });
-  }
-}
-
-chnageCorralName()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// chnageCorralName()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
